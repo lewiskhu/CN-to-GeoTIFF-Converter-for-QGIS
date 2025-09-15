337240f658fdfcfd972c64d81e80aa6becbc697f
lewiskhu <lewis@khu.ac.kr>
2025-09-15 16:19:36 +0900

    first comit

--- cn_to_tif.py
+++ cn_to_tif.py
@@ -32,6 +32,3 @@ for idx, file in enumerate(cn_files, start=1):
     })
 
 print("Conversion finished! All .nc files saved as .tif in:", output_folder)
-print("Conversion finished! All .nc files saved as .tif in:", output_folder)
-print("Conversion finished! All .nc files saved as .tif in:", output_folder)
-print("Conversion finished! All .nc files saved as .tif in:", output_folder)
